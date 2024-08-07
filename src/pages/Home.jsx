ff8c6e86b67c286c136e23037eef45b72a9f1e11
siLeNtKnIGht966 <subhransujena2468@gmail.com>
2024-08-07 09:18:35 +0530

    day1 is complete but the logo file is not completed.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
+import HeroSection from "../components/HeroSection"
 
 const Home = () => {
   return (
-    <div>
-        Home
-      
+    <div className="mt-36">
+    <HeroSection/>
     </div>
   )
 }
